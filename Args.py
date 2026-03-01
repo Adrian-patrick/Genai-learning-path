5c567f0f3f373b43a5e6b8472d5bfe1e5e4b67a4
Adrian-patrick <adrianpatrick238@gmail.com>
2026-03-02 01:42:16 +0530

    completed till functions

---
+++ Args.py
@@ -0,0 +1,8 @@
+"""
+*args : variable number of positional arguments
+**kwargs : variable number of keyword arguments
+"""
+def example_function(*args, **kwargs):
+    print("Positional arguments:", args)
+    print("Keyword arguments:", kwargs)
+example_function(1, 2, 3, name="Alice", age=30)
---
+++ Conditionals.py
@@ -0,0 +1,27 @@
+"""
+guard clauses
+validating early
+"""
+if not user:
+    raise ValueError("User not found")
+
+"""
+removing unnecessary else
+"""
+if not valid:
+    return False
+process()
+
+"""
+explicit check
+"""
+if temperatur is None:
+
+"""
+using in for multi condition checks
+"""
+if role in ["admin", "editor"]:
+
+"""
+chaining comparisons
+"""
