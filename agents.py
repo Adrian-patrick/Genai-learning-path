b14752391cde3ec3e84d72b72e7dd82a174887c6
Adrian Patrick <adrian.patrick@intimetec.com>
2026-04-16 15:16:32 +0530

    created multi agent

--- agents.py
+++ agents.py
@@ -1,12 +1,18 @@
-from pydantic_ai import Agent,RunContext
+import os
+from dataclasses import dataclass, field
+from dotenv import load_dotenv
+from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIChatModel
 from pydantic_ai.providers.azure import AzureProvider
-from dataclasses import dataclass
-import os 
-from dotenv import load_dotenv
+from pydantic_graph import BaseNode, Graph, End
+
 load_dotenv()
 
-#setting up model(azure)
+@dataclass
+class State:
+    domain: str
+    topics: dict[str, str] = field(default_factory=dict)
+
 azure_provider = AzureProvider(
     azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
     api_version=os.environ["AZURE_OPENAI_API_VERSION"],
@@ -15,44 +21,44 @@ azure_provider = AzureProvider(
 
 model = OpenAIChatModel(os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"], provider=azure_provider)
 
-
-#defining agent
-from models import Domain
-system_prompt = """
-You are an expert in topics and summary making
-Your task is to use the user query get the domain of the user which you will use to create a list of topics and summary of the domain specified
-if the user is found update the source = "TOOL"
-if the user is not found fill domain = "NOT FOUND",make source = "LLM" and make rest of the values null
-Make sure the topics are as diverse as possible
-You should also rate the importance on a scale of 1 to 10 depeneding on how important the topic is in the domain be very critical
-for the importance the lower end towards 1 means that the topic is not important in the domian and towards the higher end towards ten means that the topic is very important
-DONT always rate highly
-Your output must be structured and parseable
-RETURN only in the structured format no extra text
-"""
-
-agent = Agent(
+topic_agent = Agent(
     model=model,
-    system_prompt=system_prompt,
-    output_type=Domain,
+    system_prompt="your job is to create a list of topics of the domain limit to 2",
+    output_type=list[str],
     output_retries=3
 )
 
-#state,tools and dependency  for the agent
+summarizer_agent = Agent(
+    model=model,
+    system_prompt="you job is to summarize the topic given",
+    output_type=str,
+    output_retries=3
+)
 
-#database
 @dataclass
-class My_db():
-    db : dict
+class TopicNode(BaseNode[State]):
+    state: State
 
-#user domain
-@agent.tool
-def get_domain(ctx:RunContext[My_db],user:str) -> str:
-    """ use this tool to get the domain of the user"""
-    db_dict = ctx.deps.db
-    try:
-        user_domain = db_dict[user]
-    except Exception:
-        return "user not found so return NOT FOUND and follow instructions"
-    return user_domain
+    async def run(self, ctx) -> 'SummarizerNode':
+        print("Topic node started")
+        result = await topic_agent.run(f"topics for the domain {self.state.domain}")
+        for t in result.output:
+            self.state.topics[t] = ""
 
+        print("Topic node ended")
+        return SummarizerNode(state=self.state)
+
+@dataclass
+class SummarizerNode(BaseNode[State]):
+    state: State
+
+    async def run(self, ctx) -> End[State]:
+        print("Summarizer node started")
+        for topic in self.state.topics:
+            summary = await summarizer_agent.run(f"summarize {topic} in under 20 words")
+            self.state.topics[topic] = summary.output
+
+        print("Summarizer node ended")
+        return End(self.state)
+
+graph = Graph(nodes=[TopicNode, SummarizerNode])
