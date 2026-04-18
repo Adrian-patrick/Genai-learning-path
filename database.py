45345c9549c9aee5e4f80e71abbb174fdb6ea207
Adrian Patrick <adrianpatrick238@gmail.com>
2026-04-18 22:51:59 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ import pymysql
 
 def connect_db():
     return pymysql.connect(
-        host="127.0.0.1",
-        user="root",
-        password="root123",
-        database="library"
+        host="your_host",
+        user="your_user",
+        password="your_pass",
+        database="your_db"
     )
